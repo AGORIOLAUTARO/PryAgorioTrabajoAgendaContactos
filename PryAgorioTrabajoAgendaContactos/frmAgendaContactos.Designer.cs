10806fdfc9d8bf0f3adf12bc450272bc88622814
AGORIOLAUTARO <lautaroagorio@gmail.com>
2025-09-09 13:38:09 -0300

    Modificaciones 9/9

--- PryAgorioTrabajoAgendaContactos/frmAgendaContactos.Designer.cs
+++ PryAgorioTrabajoAgendaContactos/frmAgendaContactos.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAgendaContactos));
             lblContacto = new Label();
             lblNumero = new Label();
             btnCancelar = new Button();
@@ -35,31 +36,36 @@
             lstResumen = new ListBox();
             txtContacto = new TextBox();
             mtbNumero = new MaskedTextBox();
+            lblCantContactos = new Label();
+            lblFecha = new Label();
+            mtbCantContacto = new MaskedTextBox();
+            mtbFecha = new MaskedTextBox();
             SuspendLayout();
             // 
             // lblContacto
             // 
             lblContacto.AutoSize = true;
-            lblContacto.Location = new Point(161, 59);
+            lblContacto.Location = new Point(40, 25);
             lblContacto.Name = "lblContacto";
-            lblContacto.Size = new Size(69, 20);
+            lblContacto.Size = new Size(56, 15);
             lblContacto.TabIndex = 0;
             lblContacto.Text = "Contacto";
             // 
             // lblNumero
             // 
             lblNumero.AutoSize = true;
-            lblNumero.Location = new Point(161, 141);
+            lblNumero.Location = new Point(45, 78);
             lblNumero.Name = "lblNumero";
-            lblNumero.Size = new Size(63, 20);
+            lblNumero.Size = new Size(51, 15);
             lblNumero.TabIndex = 1;
             lblNumero.Text = "Número";
             // 
             // btnCancelar
             // 
-            btnCancelar.Location = new Point(202, 259);
+            btnCancelar.Location = new Point(45, 125);
+            btnCancelar.Margin = new Padding(3, 2, 3, 2);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(94, 29);
+            btnCancelar.Size = new Size(92, 36);
             btnCancelar.TabIndex = 2;
             btnCancelar.Text = "Cancelar";
             btnCancelar.UseVisualStyleBackColor = true;
@@ -68,9 +74,10 @@
             // btnAceptar
             // 
             btnAceptar.Enabled = false;
-            btnAceptar.Location = new Point(376, 259);
+            btnAceptar.Location = new Point(143, 125);
+            btnAceptar.Margin = new Padding(3, 2, 3, 2);
             btnAceptar.Name = "btnAceptar";
-            btnAceptar.Size = new Size(94, 29);
+            btnAceptar.Size = new Size(96, 36);
             btnAceptar.TabIndex = 3;
             btnAceptar.Text = "Agendar";
             btnAceptar.UseVisualStyleBackColor = true;
@@ -79,34 +86,76 @@
             // lstResumen
             // 
             lstResumen.FormattingEnabled = true;
-            lstResumen.Location = new Point(195, 334);
+            lstResumen.ItemHeight = 15;
+            lstResumen.Location = new Point(55, 187);
+            lstResumen.Margin = new Padding(3, 2, 3, 2);
             lstResumen.Name = "lstResumen";
-            lstResumen.Size = new Size(312, 104);
+            lstResumen.Size = new Size(189, 49);
             lstResumen.TabIndex = 4;
             // 
             // txtContacto
             // 
-            txtContacto.Location = new Point(328, 60);
+            txtContacto.Location = new Point(129, 17);
+            txtContacto.Margin = new Padding(3, 2, 3, 2);
             txtContacto.Name = "txtContacto";
-            txtContacto.Size = new Size(125, 27);
+            txtContacto.Size = new Size(110, 23);
             txtContacto.TabIndex = 5;
             txtContacto.TextChanged += txtContacto_TextChanged;
             // 
             // mtbNumero
             // 
             mtbNumero.Enabled = false;
-            mtbNumero.Location = new Point(328, 141);
+            mtbNumero.Location = new Point(129, 70);
+            mtbNumero.Margin = new Padding(3, 2, 3, 2);
             mtbNumero.Mask = "(999)000-0000";
             mtbNumero.Name = "mtbNumero";
-            mtbNumero.Size = new Size(125, 27);
+            mtbNumero.Size = new Size(110, 23);
             mtbNumero.TabIndex = 6;
             mtbNumero.MaskInputRejected += mtbNumero_MaskInputRejected;
             // 
+            // lblCantContactos
+            // 
+            lblCantContactos.AutoSize = true;
+            lblCantContactos.Location = new Point(22, 255);
+            lblCantContactos.Name = "lblCantContactos";
+            lblCantContactos.Size = new Size(81, 15);
+            lblCantContactos.TabIndex = 7;
+            lblCantContactos.Text = "CantContacto";
+            // 
+            // lblFecha
+            // 
+            lblFecha.AutoSize = true;
+            lblFecha.Location = new Point(22, 308);
+            lblFecha.Name = "lblFecha";
+            lblFecha.Size = new Size(74, 15);
+            lblFecha.TabIndex = 8;
+            lblFecha.Text = "Fecha y hora";
+            // 
+            // mtbCantContacto
+            // 
+            mtbCantContacto.Location = new Point(139, 252);
+            mtbCantContacto.Name = "mtbCantContacto";
+            mtbCantContacto.Size = new Size(100, 23);
+            mtbCantContacto.TabIndex = 9;
+            // 
+            // mtbFecha
+            // 
+            mtbFecha.Location = new Point(139, 300);
+            mtbFecha.Mask = "00/00/0000 00:00";
+            mtbFecha.Name = "mtbFecha";
+            mtbFecha.Size = new Size(100, 23);
+            mtbFecha.TabIndex = 10;
+            mtbFecha.ValidatingType = typeof(DateTime);
+            // 
             // frmAgendaContactos
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(274, 362);
+            Controls.Add(mtbFecha);
+            Controls.Add(mtbCantContacto);
+            Controls.Add(lblFecha);
+            Controls.Add(lblCantContactos);
             Controls.Add(mtbNumero);
             Controls.Add(txtContacto);
             Controls.Add(lstResumen);
@@ -114,8 +163,12 @@
             Controls.Add(btnCancelar);
             Controls.Add(lblNumero);
             Controls.Add(lblContacto);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 2, 3, 2);
             Name = "frmAgendaContactos";
-            Text = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Agenda Contactos";
+            Load += frmAgendaContactos_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -129,5 +182,9 @@
         private ListBox lstResumen;
         private TextBox txtContacto;
         private MaskedTextBox mtbNumero;
+        private Label lblCantContactos;
+        private Label lblFecha;
+        private MaskedTextBox mtbCantContacto;
+        private MaskedTextBox mtbFecha;
     }
 }
--- PryAgorioTrabajoAgendaContactos/frmAgendaContactos.cs
+++ PryAgorioTrabajoAgendaContactos/frmAgendaContactos.cs
@@ -8,6 +8,11 @@ namespace PryAgorioTrabajoAgendaContactos
         }
         string vContacto = "";
         string vNumero = "";
+        DateTime vFecha = DateTime.Now;
+        string[] vecContacto = new string[5];
+        string[] vecNumero = new string[5];
+        int indice = 0;
+        int vContador = 0;
 
         private void txtContacto_TextChanged(object sender, EventArgs e)
         {
@@ -46,8 +51,25 @@ namespace PryAgorioTrabajoAgendaContactos
         {
             vContacto = txtContacto.Text;
             vNumero = mtbNumero.Text;
+            vecContacto[indice] = mtbCantContacto.Text;
+            vecNumero[indice] = mtbFecha.Text;
 
             lstResumen.Items.Add("Contacto: " + vContacto + " - Telefono: " + vNumero);
+
+            vContador = vContador + 1;
+            mtbCantContacto.Text = Convert.ToString(vContador);
+            mtbFecha.Text = Convert.ToString(vFecha);
+
+            txtContacto.Text = "";
+            mtbNumero.Text = "";
+
+            indice++;
+            txtContacto.Focus();
+        }
+
+        private void frmAgendaContactos_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
