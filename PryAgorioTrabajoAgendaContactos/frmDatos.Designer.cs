b758454cb450c85955775b3848bcb60d3072e04a
lautaroagorio04 <lautaroagorio@gmail.com>
2025-09-15 21:19:35 -0300

    Modificaciones 15/9

---
+++ PryAgorioTrabajoAgendaContactos/frmDatos.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PryAgorioTrabajoAgendaContactos
+{
+    partial class frmDatos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAnterior = new Button();
+            btnSiguiente = new Button();
+            lblDatos = new Label();
+            SuspendLayout();
+            // 
+            // btnAnterior
+            // 
+            btnAnterior.Location = new Point(197, 223);
+            btnAnterior.Name = "btnAnterior";
+            btnAnterior.Size = new Size(94, 29);
+            btnAnterior.TabIndex = 0;
+            btnAnterior.Text = "Anterior";
+            btnAnterior.UseVisualStyleBackColor = true;
+            // 
+            // btnSiguiente
+            // 
+            btnSiguiente.Location = new Point(478, 223);
+            btnSiguiente.Name = "btnSiguiente";
+            btnSiguiente.Size = new Size(94, 29);
+            btnSiguiente.TabIndex = 1;
+            btnSiguiente.Text = "Siguiente";
+            btnSiguiente.UseVisualStyleBackColor = true;
+            btnSiguiente.Click += btnSiguiente_Click;
+            // 
+            // lblDatos
+            // 
+            lblDatos.AutoSize = true;
+            lblDatos.Location = new Point(351, 86);
+            lblDatos.Name = "lblDatos";
+            lblDatos.Size = new Size(0, 20);
+            lblDatos.TabIndex = 2;
+            // 
+            // frmDatos
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblDatos);
+            Controls.Add(btnSiguiente);
+            Controls.Add(btnAnterior);
+            Name = "frmDatos";
+            Text = "frmDatos";
+            Load += frmDatos_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAnterior;
+        private Button btnSiguiente;
+        private Label lblDatos;
+    }
+}
---
+++ PryAgorioTrabajoAgendaContactos/frmDatos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryAgorioTrabajoAgendaContactos
+{
+    public partial class frmDatos : Form
+    {
+        public frmDatos()
+        {
+            InitializeComponent();
+        }
+
+        public string[] vecContacto = new string[5];
+        int indice = 0;
+        private void frmDatos_Load(object sender, EventArgs e)
+        {
+            vecContacto[0] = "Depietri";
+            vecContacto[1] = "Catalan";
+            vecContacto[2] = "Guiñazu";
+            vecContacto[3] = "Gandolfi";
+            vecContacto[4] = "Willington";
+
+            lblDatos.Text = vecContacto[indice];
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            indice++;
+            lblDatos.Text = vecContacto[indice];
+
+            if (vecContacto.Length == (indice + 1))
+            {
+                btnSiguiente.Enabled = false;
+            }
+        }
+    }
+}
